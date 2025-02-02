644ab0609bf1b747a9d91b93286e0e33c5985e92
jorge <jorge1301@hotmail.com>
2025-02-02 18:23:16 +0100

    Restore unnecessary changes

--- src/test/java/com/backend/pricing/integration/IntegrationControllerTest.java
+++ src/test/java/com/backend/pricing/integration/IntegrationControllerTest.java
@@ -4,17 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.time.LocalDateTime;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationControllerTest {
-
-    @LocalServerPort
-    private int port;
-
     @Autowired
     private WebTestClient webTestClient;
 
